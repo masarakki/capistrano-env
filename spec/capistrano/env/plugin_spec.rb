f65716ff438855c6a1594a4f17289047de9b0280
masarakki <masaki@hisme.net>
2014-02-17 22:43:39 +0900

    set env

--- spec/capistrano/env/plugin_spec.rb
+++ spec/capistrano/env/plugin_spec.rb
@@ -5,11 +5,17 @@ describe Capistrano::Env::Plugin do
   let(:capistrano) { Capistrano::Configuration.new }
 
   describe :capenv do
-    let(:setup) { capistrano.capenv.use {|config| ; } }
+    let(:setup) { capistrano.capenv.use {|config| config.add "HELLO", "WORLD" } }
+
     it 'add task' do
       expect(capistrano.capenv).to receive(:add)
       setup
     end
     it { expect(setup).to be_a Capistrano::Env::Config }
+
+    it "set default_environment" do
+      setup
+      expect(capistrano.fetch(:default_environment)).to eq "HELLO" => "WORLD"
+    end
   end
 end
